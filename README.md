d74813a949ec353e7cdfd3ab757504a64cbfd794
Артемий Спесивцев <temana2556@yandex.ru>
2021-08-09 23:27:59 +0300

    5.2

--- README.md
+++ README.md
@@ -4,6 +4,28 @@ foodgram-project
 # Сайт:
 http://84.201.163.159:8084/
 
-# Админка:
-логин: temana
-пароль: 123
+# о чём проект:
+Проект **Foodgram** это продукторый помошник, который помогает создавать рецептры,
+делиться ими и подбирать нужные ингредиенты.
+
+# что нужно иметь, чтобы его запустить:
+Docker: https://docs.docker.com/engine/install/ubuntu automatic! 
+Docker-compose: https://docs.docker.com/compose/install automatic!
+
+# установка окружения:
+1) необходимо скопировать локально репозиторий проекта
+2) создать в рабочей директории файл `.env`
+3) заполнить файл `.env`
+
+# как запустить и как увидеть, что проект работает:
+`docker-compose up`
+Миграции: `docker-compose run web python manage.py migrate`
+Создание админа: `docker-compose run web python manage.py createsuperuser`
+
+# Технологии:
+Postgresql 10.6: https://www.postgresql.org/download automatic!
+Guinicorn 20.0.4: https://docs.gunicorn.org/en/stable/install.html automatic!
+Ngnix 1.19.8: https://www.nginx.com/resources/wiki/start/topics/tutorials/install automatic!
+
+# коротко об авторе
+My GitHub: https://github.com/JohnTamarych automatic!
--- foodgram/settings.py
+++ foodgram/settings.py
@@ -17,7 +17,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -63,23 +63,17 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'foodgram.wsgi.application'
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': os.environ.get('DB_ENGINE', default='django.db.backends.postgresql'),
+        'NAME': os.environ.get('DB_NAME', default='postgres'),
+        'USER': os.environ.get('POSTGRES_USER', default='postgres'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD', default='1234qwerty'),
+        'HOST': os.environ.get('DB_HOST', default='db'),
+        'PORT': os.environ.get('DB_PORT', default='5432'),
     }
 }
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': os.environ.get('DB_ENGINE', default='django.db.backends.postgresql'),
-#         'NAME': os.environ.get('DB_NAME', default='postgres'),
-#         'USER': os.environ.get('POSTGRES_USER', default='postgres'),
-#         'PASSWORD': os.environ.get('POSTGRES_PASSWORD', default='1234qwerty'),
-#         'HOST': os.environ.get('DB_HOST', default='db'),
-#         'PORT': os.environ.get('DB_PORT', default='5432'),
-#     }
-# }
 
 
 AUTH_PASSWORD_VALIDATORS = [
